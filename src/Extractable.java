7cc696009fb70b73ae43a26c62eedf7e3a94f9d8
_Mi.n0u_ <105317987+Min0u@users.noreply.github.com>
2023-10-28 16:33:42 +0200

    Delete src directory

--- src/Extractable.java
+++
@@ -1,6 +0,0 @@
-import java.util.List;
-import java.util.Objects;
-
-public interface Extractable<T> {
-    public void extract(T type);
-}
--- src/Instrument.java
+++
@@ -1,3 +0,0 @@
-public interface Instrument {
-    public void jouerSignal(Notes note);
-}
