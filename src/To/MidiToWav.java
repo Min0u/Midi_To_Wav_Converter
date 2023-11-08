d3c7ceb9e0558fc35fa0f74bceacca2ceba72f96
_Mi.n0u_ <105317987+Min0u@users.noreply.github.com>
2023-11-08 23:02:08 +0100

    Add files via upload

---
+++ src/To/MidiToWav.java
@@ -0,0 +1,38 @@
+package To;
+
+import Midi.Event;
+import Midi.FichierMidi;
+import Midi.Message;
+import Midi.NoteJouee;
+import Wav.FichierWav;
+import Wav.Signal;
+
+public class MidiToWav {
+
+    private MidiToWav() {
+    }
+
+    public static void convert(String MidiFile, String WavFile) {
+        // Sequence, resolution
+        FichierMidi f = new FichierMidi(MidiFile);
+
+        // Events
+        Event event = new Event(f);
+        event.extract();
+
+        // Messages
+        Message messages = new Message(f, event.getEvents());
+        messages.extract();
+
+        // totalTime, Notes
+        float totalTime = messages.calculTotalTime();
+        f.setTotalTime(totalTime);
+
+        // Signal
+        Signal s = new Signal();
+        int[] signal = s.genererSignal(messages.getNotes(), f.getTotalTime());
+
+        // FichierWav
+        FichierWav.ecrireFichierWav(WavFile, signal);
+    }
+}
