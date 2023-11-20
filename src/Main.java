ec1156d28d147ea33ee742ff2cab87bc3e85a232
_Mi.n0u_ <105317987+Min0u@users.noreply.github.com>
2023-11-20 15:29:08 +0100

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import To.*;
+
+public class Main {
+    public static void main(String[] args) {
+        MidiToWav.convert("fichier1.mid","fichier1.wav");
+        MidiToWav.convert("fichier2.mid","fichier2.wav");
+        MidiToWav.convert("fichier3.mid","fichier3.wav");
+    }
+}
---
+++ src/Wav/FichierWav.java
@@ -0,0 +1,57 @@
+package Wav;
+
+import Midi.FichierMidi;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+
+public class FichierWav {
+    // Attributs
+    private static FichierMidi fichierMidi;
+
+    // Constructeur
+    private FichierWav() {
+    }
+
+    // Setters
+    public static void setFichierMidi(FichierMidi fichierMidi) {
+        FichierWav.fichierMidi = fichierMidi;
+    }
+
+    // Méthodes
+    public static byte[] convertirEnOctets(int[] signal) {
+        int n = signal.length;
+        byte[] bytes = new byte[n * 4]; // Chaque entier est de 4 octets
+
+        for (int i = 0; i < n; i++) {
+            int entier = signal[i];
+
+            bytes[i * 4] = (byte) (entier >> 24);
+            bytes[i * 4 + 1] = (byte) (entier >> 16);
+            bytes[i * 4 + 2] = (byte) (entier >> 8);
+            bytes[i * 4 + 3] = (byte) entier;
+        }
+
+        return bytes;
+    }
+
+    public static void ecrireFichierWav(String nomFichier, int[] signal) {
+        try {
+            AudioFormat format = new AudioFormat(44100, 8, 1, true, true);
+            byte[] b = convertirEnOctets(signal);
+            AudioInputStream ais = new AudioInputStream(new ByteArrayInputStream(b), format, b.length);
+            File file = new File(nomFichier);
+            AudioSystem.write(ais, AudioFileFormat.Type.WAVE, file);
+            if (fichierMidi.isLoaded()) {
+                System.out.println("Fichier WAV enregistré avec succès : " + file.getName() + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Erreur lors de la lecture du fichier");
+        }
+    }
+}
